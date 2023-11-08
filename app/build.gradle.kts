6769d120ec068730d225959261be9037044be495
lk <lkgiovani@gmail.com>
2023-11-08 00:11:26 -0300

    Colocando o RecyclerView
    depois da uma olhada em como deve ficar o layout da linha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    viewBinding{
+        enable = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
--- app/src/main/java/com/example/app_kairos_projeto01/HomeActivity.kt
+++ app/src/main/java/com/example/app_kairos_projeto01/HomeActivity.kt
@@ -4,14 +4,18 @@ package com.example.app_kairos_projeto01
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.app_kairos_projeto01.databinding.HomeActivityBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding:HomeActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.home_activity)
+        binding = HomeActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         var buttonCalendario: Button = findViewById(R.id.button3)
         var buttonPontos: Button = findViewById(R.id.button4)
@@ -30,6 +34,30 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, PontosActivity::class.java)
             startActivity(intent)
         }
-
+        initRecyclerView()
     }
+    private fun initRecyclerView(){
+        binding.RecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.RecyclerView.setHasFixedSize(true)
+        binding.RecyclerView.adapter = Adpter(getList())
+    }
+
+    private fun getList() = listOf(
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani",
+        "giovani"
+    )
+
 }
